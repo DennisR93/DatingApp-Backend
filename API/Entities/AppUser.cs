c40e3c8c8ef0d3bce45a7dfd952694af0f1d3750
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-13 15:13:50 +0200

    adding comment

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,9 +1,13 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Entities
 {
     public class AppUser
     {
         public int Id { get; set; }
+        
+        // [Required] Possible way to make it required 
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
