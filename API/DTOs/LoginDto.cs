beddddf0435c02780aa3c471684626abb2a50046
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-13 16:53:32 +0200

    creating login Dto

---
+++ API/DTOs/LoginDto.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
+
+public class LoginDto
+{
+   [Required]
+   [MinLength(3)]
+   [MaxLength(8)]
+   public string Username { get; set; }
+   
+   [Required]
+   [MinLength(4)]
+   [MaxLength(10)]
+   public string Password { get; set; }
+}
