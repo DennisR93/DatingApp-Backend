559f5cafbff372b20b6e3f3726474906d3d609fa
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-18 17:11:33 +0200

    removing try catch

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -34,19 +34,11 @@ public class BuggyController : BaseApiController
     [HttpGet("server-error")]
     public ActionResult<string> GetServerError()
     {
-        try
-        {
-            var thing = _context.Users.Find(-1);
+        var thing = _context.Users.Find(-1);
 
-            var thingToReturn = thing.ToString();
+        var thingToReturn = thing.ToString();
 
-            return thingToReturn;
-        }
-        catch (Exception ex)
-        {
-            return StatusCode(500, "Computer says no!");
-        }
-        
+        return thingToReturn;
     }
     
     [HttpGet("bad-request")]
