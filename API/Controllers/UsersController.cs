bb16fa58953261f9debb6032fb8b2da9a8070796
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-21 11:31:17 +0200

    adding HttpPost to photos

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -53,6 +53,33 @@ namespace API.Controllers
 
             return BadRequest("Failed to update user");
         }
+        
+        [HttpPost("add-photo")]
+        public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
+        {
+            var username = User.GetUsername();
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            if (user == null) return NotFound();
+
+            var result = await _photoService.AddPhotoAsync(file);
+
+            if (result.Error != null) return BadRequest(result.Error.Message);
+
+            var photo = new Photo
+            {
+                Url = result.SecureUrl.AbsoluteUri,
+                PublicId = result.PublicId
+            };
+
+            if (user.Photos.Count == 0) photo.IsMain = true;
+            
+            user.Photos.Add(photo);
+
+            if (await _userRepository.SaveAllAsync()) return _mapper.Map<PhotoDto>(photo);
+
+            return BadRequest("Problem adding photo");
+        }
     }
 }
 
