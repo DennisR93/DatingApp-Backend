d71c15d06de4d24b00949b0835eac0da652a5b48
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-21 11:22:18 +0200

    adding Getusername method to controller

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,25 +3,26 @@ using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    // [AllowAnonymous] will bypass all
     [Authorize]
    public class UsersController : BaseApiController
     {
         private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
+        private readonly IPhotoService _photoService;
 
-        public UsersController(IUserRepository userRepository, IMapper mapper)
+        public UsersController(IUserRepository userRepository, IMapper mapper, IPhotoService photoService)
         {
             _userRepository = userRepository;
             _mapper = mapper;
+            _photoService = photoService;
         }
 
         [HttpGet]
@@ -31,14 +32,6 @@ namespace API.Controllers
 
             return Ok(users);
         }
-        // Old
-        // public async Task <ActionResult<IEnumerable<MemberDto>>> GetUsers()
-        // {
-        //     var users = await _userRepository.GetUsersAsync();
-        //     var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
-        //
-        //     return Ok(usersToReturn);
-        // }
 
         [HttpGet("{username}")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
@@ -46,25 +39,10 @@ namespace API.Controllers
             return await _userRepository.GetMemberAsync(username);
         }
         
-        // Old
-        // public async Task<ActionResult<MemberDto>> GetUser(string username)
-        // {
-        //     var user = await _userRepository.GetUserByUsernameAsync(username);
-        //
-        //     return _mapper.Map<MemberDto>(user);
-        // }
-
-        // [Authorize] no need because there is authorize on top level
-        // [HttpGet("{id}")] // /api/users/2
-        // public async Task <ActionResult<AppUser>> GetUser(int id)
-        // {
-        //     return await _context.Users.FindAsync(id);
-        // }
-
         [HttpPut]
         public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
         {
-            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var username = User.GetUsername();
             var user = await _userRepository.GetUserByUsernameAsync(username);
 
             if (user == null) return NotFound();
