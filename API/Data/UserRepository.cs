6be8736dcec71d1cb9a1f9fd31ebc345d3cfc986
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-22 16:41:20 +0200

    updating repository

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -1,5 +1,6 @@
 using API.DTOs;
 using API.Entities;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
@@ -43,9 +44,10 @@ public class UserRepository : IUserRepository
         return await _context.Users.Include(p => p.Photos).SingleOrDefaultAsync(u => u.UserName == username);
     }
 
-    public async Task<IEnumerable<MemberDto>> GetMembersAsync()
+    public async Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams)
     {
-        return await _context.Users.ProjectTo<MemberDto>(_mapper.ConfigurationProvider).ToListAsync();
+        var query = _context.Users.ProjectTo<MemberDto>(_mapper.ConfigurationProvider).AsNoTracking();
+        return await PagedList<MemberDto>.CreateAsync(query, userParams.PageNumber, userParams.PageSize);
     }
 
     public async Task<MemberDto> GetMemberAsync(string username)
