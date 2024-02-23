eeace9ac4b8a60f7c86e7902713aa98af1e72b15
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-23 20:44:07 +0200

    fixing to use likesParams

--- API/Data/LikesRepository.cs
+++ API/Data/LikesRepository.cs
@@ -1,6 +1,7 @@
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
@@ -24,24 +25,24 @@ public class LikesRepository : ILikesRepository
         return await _context.Users.Include(l => l.LikedUsers).FirstOrDefaultAsync(u => u.Id == userId);
     }
 
-    public async Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId)
+    public async Task<PagedList<LikeDto>> GetUserLikes(LikesParams likesParams)
     {
         var users = _context.Users.OrderBy(u => u.UserName).AsQueryable();
         var likes = _context.Likes.AsQueryable();
 
-        if (predicate == "liked")
+        if (likesParams.Predicate == "liked")
         {
-            likes = likes.Where(like => like.SourceUserId == userId);
+            likes = likes.Where(like => like.SourceUserId == likesParams.UserId);
             users = likes.Select(like => like.TargetUser);
         }
         
-        if (predicate == "likedBy")
+        if (likesParams.Predicate == "likedBy")
         {
-            likes = likes.Where(like => like.TargetUserId == userId);
+            likes = likes.Where(like => like.TargetUserId == likesParams.UserId);
             users = likes.Select(like => like.SourceUser);
         }
 
-        return await users.Select(user => new LikeDto
+        var likedUsers = users.Select(user => new LikeDto
         {
             UserName = user.UserName,
             KnownAs = user.KnownAs,
@@ -49,6 +50,8 @@ public class LikesRepository : ILikesRepository
             PhotoUrl = user.Photos.FirstOrDefault(p => p.IsMain).Url,
             City = user.City,
             Id = user.Id
-        }).ToListAsync();
+        });
+
+        return await PagedList<LikeDto>.CreateAsync(likedUsers, likesParams.PageNumber, likesParams.PageSize);
     }
 }
