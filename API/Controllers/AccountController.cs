e46c23147fccc50ed19a62bae0dba5ef851a7be9
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-25 16:13:13 +0200

    adding role to token

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -37,11 +37,15 @@ public class AccountController : BaseApiController
         var result = await _userManager.CreateAsync(user, registerDto.Password);
 
         if (!result.Succeeded) return BadRequest(result.Errors);
+
+        var roleResult = await _userManager.AddToRoleAsync(user, "Member");
+
+        if (!roleResult.Succeeded) return BadRequest(result.Errors);
         
         return new UserDto
         {
             Username = user.UserName,
-            Token = _tokenService.CreateToken(user),
+            Token = await _tokenService.CreateToken(user),
             KnownAs = user.KnownAs,
             Gender = user.Gender
         };
@@ -61,7 +65,7 @@ public class AccountController : BaseApiController
         return new UserDto
         {
             Username = user.UserName,
-            Token = _tokenService.CreateToken(user),
+            Token = await _tokenService.CreateToken(user),
             PhotoUrl = user.Photos.FirstOrDefault(p => p.IsMain)?.Url,
             KnownAs = user.KnownAs,
             Gender = user.Gender
