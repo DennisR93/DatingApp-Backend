ffc346c674bd35a09eae02e79b79ee1ca92eee0f
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-25 10:13:37 +0200

    adding delete to query

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -39,9 +39,9 @@ public class MessageRepository : IMessageRepository
 
         query = messageParams.Container switch
         {
-            "Inbox" => query.Where(u => u.RecipientUsername == messageParams.Username),
-            "Outbox" => query.Where(u => u.SenderUsername == messageParams.Username),
-            _ => query.Where(u => u.RecipientUsername == messageParams.Username && u.DateRead == null)
+            "Inbox" => query.Where(u => u.RecipientUsername == messageParams.Username && u.RecipientDeleted == false),
+            "Outbox" => query.Where(u => u.SenderUsername == messageParams.Username && u.SenderDeleted == false),
+            _ => query.Where(u => u.RecipientUsername == messageParams.Username && u.RecipientDeleted == false && u.DateRead == null)
         };
 
         var messages = query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider);
@@ -52,8 +52,8 @@ public class MessageRepository : IMessageRepository
     public async Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername, string recipientUsername)
     {
         var messages = await _context.Messages.Include(u => u.Sender).ThenInclude(p => p.Photos).Where(m =>
-                m.RecipientUsername == currentUsername && m.SenderUsername == recipientUsername ||
-                m.RecipientUsername == recipientUsername && m.SenderUsername == currentUsername)
+                m.RecipientUsername == currentUsername && m.RecipientDeleted == false && m.SenderUsername == recipientUsername ||
+                m.RecipientUsername == recipientUsername && m.SenderDeleted == false && m.SenderUsername == currentUsername)
             .OrderBy(m => m.MessageSent)
             .ToListAsync();
 
