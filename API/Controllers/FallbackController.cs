6bff44f4b2700a4585d27737fffb0ef90e8844bd
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-27 00:11:31 +0200

    comment

--- API/Controllers/FallbackController.cs
+++ API/Controllers/FallbackController.cs
@@ -7,5 +7,6 @@ public class FallbackController : Controller
     public ActionResult Index()
     {
         return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "root", "index.html"), "text/HTML");
+        //NEED TO CHANGE "root" to real DIRECTORY!
     }
 }
