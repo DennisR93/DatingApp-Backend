9d06e19eb320cc7a17f05124e5b78288e4081423
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-26 20:11:41 +0200

    adding task to find gender

--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -13,4 +13,5 @@ public interface IUserRepository
 
     Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams);
     Task<MemberDto> GetMemberAsync(string username);
+    Task<string> GetUserGender(string username);
 }
