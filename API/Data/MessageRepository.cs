5dfab6551be74bf0f5c789bd1263e77bcee095ae
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-26 19:55:39 +0200

    removing save async

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -65,8 +65,6 @@ public class MessageRepository : IMessageRepository
             {
                 message.DateRead = DateTime.UtcNow;
             }
-
-            await _context.SaveChangesAsync();
         }
 
         return _mapper.Map<IEnumerable<MessageDto>>(messages);
